c9450d649dc68f4bb04d5c3d5de4a97095ed3503
Antonin06 <avon.antonin@gmail.com>
2020-06-15 16:44:28 +0200

    Antonin

---
+++ view/home.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <title>Home</title>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-white font-weight-bold homebar p-2">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link text-dark" href="#">Nos Destinations</a>
+        <img src="../assets/images/travel-transparent.png" width="154" height="70" class="" alt="">
+        <a class="nav-item nav-link text-dark" href="#">Les Tours Operateurs</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+      <h1 class="display-4">Promotional banner</h1>
+      <p class="lead">Créer differentes destinations</p>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <h2 class="text-center mb-4">Nos destinations</h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card border-dark shadow" style="width: 18rem;">
+          <img src="https://source.unsplash.com/nnzkZNYWHaU/300x300" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <button type="button" class="btn btn-outline-secondary">Paris</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card border-dark shadow" style="width: 18rem;">
+          <img src="https://source.unsplash.com/EXdXLrZXS9Q/300x300" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <button type="button" class="btn btn-outline-secondary">Londres</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card border-dark shadow" style="width: 18rem;">
+          <img src="https://source.unsplash.com/random/300x300/?Barcelone" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <button type="button" class="btn btn-outline-secondary">Barcelone</button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
