5d2690d18ea079a4de461865571fa7dae0492d83
Antonin06 <avon.antonin@gmail.com>
2020-06-19 12:20:29 +0200

    modif navbar admin master

--- assets/partials/nav-master-admin.php
+++ assets/partials/nav-master-admin.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand text-white" href="../view/master-admin.php">ADMINISTRATOR MASTER</a>
+  <a class="navbar-brand text-white" href="../master-admin/master-admin.php">ADMINISTRATOR MASTER</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
