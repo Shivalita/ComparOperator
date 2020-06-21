988fe297d9ed629b0339b46e8aac1b968141b07a
Antonin06 <avon.antonin@gmai.com>
2020-06-21 13:11:39 +0200

    peaufinage v2

--- assets/partials/nav-user.php
+++ assets/partials/nav-user.php
@@ -5,7 +5,7 @@
   <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link text-dark mt-2" href="destinations.php">Nos Destinations</a>
-      <a href="home.php"><img src="../assets/images/travel-transparent.png" width="154" height="70" class="mt-0" alt=""></a>
+      <a href="home.php"><img src="../assets/images/travel-transparent.png" width="154" height="70" class="" alt=""></a>
       <a class="nav-item nav-link text-dark mt-2" href="operators.php">Les Tours Operateurs</a>
     </div>
   </div>
